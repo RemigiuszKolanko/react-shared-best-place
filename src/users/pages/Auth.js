2c984925be8aff0382496e55520da90b68be8347
Remigiusz Kolanko <remigiusz.kolanko@synergycodes.com>
2020-03-19 22:34:47 +0100

    image upload added

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -9,6 +9,7 @@ import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../s
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 
 import './Auth.css';
 
@@ -33,13 +34,18 @@ const Auth = () => {
             setFormData(
                 {
                     ...formState.inputs,
-                    name: undefined
+                    name: undefined,
+                    image: undefined
                 }, formState.inputs.email.isValid && formState.inputs.password.isValid)
         } else {
             setFormData(
                 {
                     ...formState.inputs,
                     name: {
+                        value: '',
+                        isValid: false
+                    },
+                    image: {
                         value: '',
                         isValid: false
                     }
@@ -52,6 +58,8 @@ const Auth = () => {
 
     const authSubmitHandler = async event => {
         event.preventDefault();
+
+        console.log(formState.inputs);
 
         if (isLoginMode) {
             try {
@@ -67,8 +75,8 @@ const Auth = () => {
                     }
                 );
                 auth.login(responseData.user.id);
-            } catch (err) {}
-            
+            } catch (err) { }
+
         } else {
             try {
                 const responseData = await sendRequest(
@@ -84,7 +92,7 @@ const Auth = () => {
                     }
                 );
                 auth.login(responseData.user.id);
-            } catch (err) {}
+            } catch (err) { }
         }
     }
 
@@ -97,6 +105,7 @@ const Auth = () => {
                 <hr />
                 <form onSubmit={authSubmitHandler}>
                     {!isLoginMode && <Input element="input" id="name" type="text" label="Your Name" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a name" onInput={inputHandler} />}
+                    {!isLoginMode && <ImageUpload id="image" center onInput={inputHandler} />}
                     <Input element="input" id="email" type="email" label="E-Mail" validators={[VALIDATOR_EMAIL()]} errorText="Please enter a valid email address" onInput={inputHandler} />
                     <Input element="input" id="password" type="password" label="Password" validators={[VALIDATOR_MINLENGTH(6)]} errorText="Please enter a valid password, at least 6 characters." onInput={inputHandler} />
                     <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? 'LOGIN' : 'SINGUP'}</Button>
